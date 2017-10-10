8964c34f1e1231b5afed05abde92dc34feb2476e
SanthoshYellishetty <syellishetty@csc.com>
2017-10-10 21:01:16 +0530

    Changes to Sidebar

--- OTIS_POC/App_Start/BundleConfig.cs
+++ OTIS_POC/App_Start/BundleConfig.cs
@@ -8,11 +8,17 @@ namespace OTIS_POC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/3rdpartyjs").Include(
+                        "~/Scripts/sidebar-menu.js",
+                        "~/Scripts/custom.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -25,7 +31,16 @@ namespace OTIS_POC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/sidebar-menu.css"));
+
+            bundles.Add(new StyleBundle("~/Content/3rdpartycss").Include(
+                                            "~/Content/sidebar-menu.css"));
+
+            bundles.Add(new StyleBundle("~/Content/fontawesome",
+                "https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"));
+
+           
         }
     }
 }
